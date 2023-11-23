d80839b0266a8887269a1cb40b44bbb351827f92
devlopersabbir <devlopersabbir@gmail.com>
2023-11-23 16:16:39 +0600

    if elif else condition

---
+++ crash_course/if_conditions.py
@@ -0,0 +1,35 @@
+# a = 20
+# b = 30
+
+# result = a == b  # a r b jodi soman hoy taholei amader True debe
+
+# if result:
+#     print("2 tai soman")
+
+# if a != b:
+#     print("2 tai soman na")
+
+# num1 = 100
+# num2 = 200
+
+# if num1 == num2:
+#     print("num1 and num2 is same")
+# elif num1 != num2:
+#     print("num1 and num2 is not smae")
+# elif num1 > num2:
+#     print("num1 boro num2 ar theke")
+# else:
+#     print("num1 choto num2 ar theke")
+
+
+# t = 22
+# s = 20
+# if t == s:
+#     print("t r s soman")
+
+
+a = 200
+b = 33
+c = 500
+if a > b or c > a:
+    print("Both conditions are True")
