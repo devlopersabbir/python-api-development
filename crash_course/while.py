72dc892f0956c0c9437004730a6fcab7c6f8294b
devlopersabbir <devlopersabbir@gmail.com>
2023-11-23 16:25:04 +0600

    while loop in python

---
+++ crash_course/while.py
@@ -0,0 +1,14 @@
+starting_point = 1
+
+while starting_point < 10:
+    print(starting_point)
+    starting_point += 1
+    # starting_point = starting_point + 1
+
+
+index = 1
+while True:
+    print(index)
+    index += 1
+    if index == 10:
+        break
