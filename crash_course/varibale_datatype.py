6156a0e6fa93d8040639a9ec527dae4a63d1c0ec
devlopersabbir <devlopersabbir@gmail.com>
2023-11-23 16:03:47 +0600

    boolean data type

---
+++ crash_course/varibale_datatype.py
@@ -0,0 +1,32 @@
+"""
+String Data Type
+"""
+name = "Sabbir"  # string
+fndName = 'Rafi'  # string str
+
+print(type(name))  # output: str
+print(fndName)
+
+"""
+Number Data Type
+"""
+age = 20
+print(age)
+
+"""
+list Data Type
+"""
+rolls = [20, 30, 40, 50]  # list
+print(rolls)
+
+"""
+dict Data Type
+"""
+fnds = {"Tahsin", "Rafi", "Tanvir", "Ankon"}
+print(fnds)
+
+"""
+Bool Data Type
+"""
+isOpen = False | True
+print(isOpen)
