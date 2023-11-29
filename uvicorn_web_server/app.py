2bbc46d65c1df629643975e9ac18cd319551fb01
devlopersabbir <devlopersabbir@gmail.com>
2023-11-29 21:16:21 +0600

    👍 uvicorn web server 🤷‍♂️

--- uvicorn_web_server/app.py
+++ uvicorn_web_server/app.py
@@ -3,15 +3,15 @@ async def app(scope, receive, send):
 
     try:
         await send({
-            "type": 'http.response.start',
+            "type": "http.response.start",
             "status": 200,
-            "header": [
-                [b"content-type", b"text-plain"],
+            "headers": [
+                [b"content-type", b"text-plain"]
             ]
         })
         await send({
             "type": "http.response.body",
-            "body": b"<h1>Hello World</h1>"
+            "body": b'<div><h1>hello h1</h1><h2>hello h2</h2></div>'
         })
     except:
-        print("Fail to run server")
+        print("Fail to start server!")
