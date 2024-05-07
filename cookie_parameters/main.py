46c7177af6e06e2fed6df4d914d2e19097e550c2
devlopersabbir <devlopersabbir@gmail.com>
2024-05-08 05:01:16 +0600

    [chore 👱‍♂️] cookie parameter 👲

--- cookie_parameters/main.py
+++ cookie_parameters/main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Cookie, Response
+from typing import Annotated
+
 
 app = FastAPI()
 
 @app.get("/")
 async def room_routes():
     return {"message": "hello world"}
+
+
+@app.get("/token", tags=["token"])
+async def write_token():
+    response = Response(content="cookie is set successfully")
+    response.set_cookie(key="token", value="fake-cookie-token", httponly=True, max_age=5,  path="/", secure=False, samesite="strict")
+    return response
+
+@app.get("/items", tags=["items"])
+async def read_items(token: Annotated[str | None, Cookie()] = None):
+    print(token)
+    return {"message": "hello items"}
