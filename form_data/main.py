221962655db4ce6147ae0a4026bf241c80da0e72
devlopersabbir <devlopersabbir@gmail.com>
2024-05-30 07:31:02 +0600

    [feat] entire form as a request

--- form_data/main.py
+++ form_data/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Form
+from fastapi import Request
 from typing import Annotated
 
 app = FastAPI()
@@ -7,4 +8,13 @@ app = FastAPI()
 def read_root(username: Annotated[str, Form()], password: Annotated[str, Form()]):
     print(username)
     print(password)
+    return {"Hello": "World"}
+
+
+@app.post("/")
+async def read_root(request: Request):
+    form = await request.form()
+    formData = dict(form)
+
+    print(formData["name"])
     return {"Hello": "World"}
